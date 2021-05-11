18f17629831ffbefd0291c3a092c1be29d44b610
harikrishnarudra <harikrishnarudra@gmail.com>
2021-05-11 10:24:38 +0530

    Linear DP 3 problems

---
+++ src/com/test/leetcode/dp/linearDp/CoinChange.java
@@ -0,0 +1,73 @@
+package com.test.leetcode.dp.linearDp;
+
+import java.util.Arrays;
+
+/**
+ * 322. Coin Change
+ *
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ * Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ * You may assume that you have an infinite number of each kind of coin.
+ *
+ *Example 1:
+ *
+ * Input: coins = [1,2,5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ *
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * Example 3:
+ *
+ * Input: coins = [1], amount = 0
+ * Output: 0
+ * Example 4:
+ *
+ * Input: coins = [1], amount = 1
+ * Output: 1
+ * Example 5:
+ *
+ * Input: coins = [1], amount = 2
+ * Output: 2
+ *
+ * https://www.youtube.com/watch?v=-NTaXJ7BBXs
+ */
+public class CoinChange {
+
+    public static void main(String[] args) {
+        int amount = 6249;
+        int coins[] = {186, 419, 83, 408};
+
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        int minCoinsNeeded = coinChange(coins,amount, dp);
+        System.out.println(minCoinsNeeded);
+        for (int x : dp) {
+            System.out.print(x+" ");
+        }
+    }
+    static int coinChange(int[] coins, int amount, int[] dp) {
+        if(amount ==0 ) return 0;
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<coins.length; i++){
+            if(amount - coins[i] >=0) {
+                int minAns = 0;
+                if(dp[amount-coins[i]] != -1){
+                    minAns = dp[amount - coins[i]];
+                }
+                else {
+                    minAns = coinChange(coins, amount - coins[i], dp);
+                }
+                if (minAns!= Integer.MAX_VALUE && minAns+1 < ans) {
+                    ans = minAns +1;
+                }
+            }
+        }
+
+        return dp[amount] = ans;
+    }
+}
---
+++ src/com/test/leetcode/dp/linearDp/PerfectSquares.java
@@ -0,0 +1,45 @@
+package com.test.leetcode.dp.linearDp;
+
+/**
+ * 279. Perfect Squares
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ *
+ * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ *
+ * Example 1:
+ *
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ * Example 2:
+ *
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+public class PerfectSquares {
+
+    public int numSquares(int n) {
+        if(n>0){
+            int[] dp = new int[n+1];
+            for(int i=0;i<=n;i++){
+                dp[i] = i;
+            }
+            for(int i=2;i<=n;i++){
+                for(int j=1; (i-(j*j))>=0; j++){
+                    dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+                }
+            }
+            return dp[n];
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new PerfectSquares().numSquares(35));
+        double val = 20.0;
+        int f = 10;
+        double res = f / 100.0 + val;
+        System.out.println(res);
+    }
+}
